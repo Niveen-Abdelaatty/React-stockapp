4fc40f405ccf65de06e87d3d8ca1722feca38390
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-03-21 15:09:05 -0400

    define app routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Stock from './components/Stock';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Stock App</h1>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/stocks/:symbol' element={<Stock />} />
+        <Route path='/stocks' element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
